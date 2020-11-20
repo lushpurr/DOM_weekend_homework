02162d2a33a9c145adfc21252bd4107cec19db15
lushpurr <lushpurr@gmail.com>
2020-11-20 15:16:32 +0000

    spelling mistake in css title

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newBreakfastItem = document.querySelector('#breakfast-form');
     newBreakfastItem.addEventListener('submit', handleNewBreakfastItem);
+    console.log('Breakfast Creator Initialised');
 })
