53a7a23ab85b45695211729fae4022408d19e63e
lushpurr <lushpurr@gmail.com>
2020-11-20 17:26:01 +0000

    choices listing in web page

--- js/app.js
+++ js/app.js
@@ -1,21 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Breakfast Creator Initialised');
 
-    const firstFood = document.querySelector('#first-food');
-    firstFood.addEventListener('input' , handleFirstFood);
+    const newBreakfastChoice = document.querySelector('#breakfast-form');
+    newBreakfastChoice.addEventListener('submit', handleBreakfastChoiceSubmit)
+
+    // const firstFood = document.querySelector('#first-food');
+    // firstFood.addEventListener('input' , handleFirstFood);
 
 
     });
 
-
-
-
-const handleFirstFood = function (event) {
+const handleBreakfastChoiceSubmit = function(event) {
     console.log(event);
-    const NewBreakfastChoice = document.createElement('li');
-    NewBreakfastChoice.textContent = event.target.value ; 
+
+    event.preventDefault();
+
+    const breakfastListItem = createBreakfastListItem(event.target);
+
+    const breakfastList = document.querySelector('#breakfast-list'); 
+    breakfastList.appendChild(breakfastListItem);
+    event.target.reset(); // empties our form
+}
+
+const createBreakfastListItem = function (form) {
+    const breakfastListItem = document.createElement('li'); // creates our li element
+    breakfastListItem.classList.add('breakfast-list-item'); // adds our breakfast item, and gives it a class
+
+    const firstFood = document.createElement('h2');
+    firstFood.textContent = form.firstFood.value; // tried to use first-food here as id and didnt like it
+    console.log(firstFood);
+    breakfastListItem.appendChild(firstFood);
+
+    const secondFood = document.createElement('h2');
+    console.log(secondFood);
+
+    secondFood.textContent = form.secondFood.value;
+    breakfastListItem.appendChild(secondFood);
+
+    const sauce = document.createElement('h2');
+    sauce.textContent = form.sauce.value;
+    breakfastListItem.appendChild(sauce);
+
+    return breakfastListItem;
+
+
+
 
 };
+
 
 
             // const outputSpan = document.querySelector('#awesome-output');
